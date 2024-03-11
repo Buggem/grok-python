9ebd4c212365fd9ef132513944b05ddbbc026f33
Max Parry <66670928+Buggem@users.noreply.github.com>
2024-03-11 15:02:47 +1100

    Create mad-libs-1.py

---
+++ p1/simple-games/mad-libs-1.py
@@ -0,0 +1,15 @@
+name  = input("Name: ")
+rela  = input("Relation: ")
+noun  = input("Noun: ")
+anip  = input("Animal (plural): ")
+sport = input("Sport: ")
+adjec = input("Adjective: ")
+adje2 = input("Another Adjective: ")
+verb  = input("Verb: ")
+print(f"""Dear {rela},
+Camp {noun} has been {adjec} so far!
+Tomorrow we will play {sport} if the weather is ok.
+Today it has been raining cats and {anip} all day!
+If we cannot play {sport}, maybe we will just {verb}.
+I am sure it will be {adje2} either way!
+See you soon! {name}""")
